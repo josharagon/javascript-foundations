2d418adb2564a9151d9b0a7d3bebe7a56254d2a0
Josh Aragon <josharagon99@gmail.com>
2021-05-06 21:24:29 -0600

    Complete mythical creatures

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,25 @@
 
+class Dragon {
+constructor(name, rider){
+
+  this.name = name
+  this.rider = rider
+  this.hungry = true
+  this.timesAte = 0
+  }
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+
+  eat() {
+    this.timesAte += 1;
+    if (this.timesAte >= 3) {
+      this.hungry = false;
+    } else {
+      return;
+    }
+  }
+
+}
+
+module.exports = Dragon
